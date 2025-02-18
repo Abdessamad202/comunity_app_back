5d476b624efbd7cac1619c01e04c58b215991659
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-18 02:12:52 +0100

    Add ProfileRegistrationRequest.php

---
+++ app/Http/Requests/Guest/ProfileRegistrationRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Guest;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProfileRegistrationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'sex' => 'required|in:M,F',
+            // more than 18 years
+            'date_of_birth' => 'required|date|before_or_equal:18years',
+        ];
+    }
+}
