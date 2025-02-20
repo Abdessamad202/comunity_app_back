1e5ad23bc470f0c66849bffb0509fc1af3bbab45
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-20 02:48:54 +0100

    Update app/Models/Like.php: [Describe the changes. E.g., Added new methods, modified existing methods]

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
     //
-    public function user(){
-        return $this->belongsTo(User::class);
+    use HasFactory;
+    public function profile(){
+        return $this->belongsTo(Profile::class);
     }
     public function post(){
         return $this->belongsTo(Post::class);
