952f2639c98a4bd802ad75c60b2a6ebee346a976
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-20 02:48:54 +0100

    Update app/Models/Profile.php: [Describe the changes. E.g., Added new attributes, modified existing methods]

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
     //
+    use HasFactory;
     protected $fillable = [
         'user_id','name','sex','date_of_birth',
     ];
