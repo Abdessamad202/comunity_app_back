7f1f67d1c007f2c74011ab8ffa07f21ccfd75f9e
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-20 02:48:54 +0100

    Update app/Models/Post.php: [Describe the changes. E.g., Added new relationships, modified existing relationships]

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     //
-    public function user(){
-        return $this->belongsTo(User::class);
+    use HasFactory;
+    public function profile(){
+        return $this->belongsTo(Profile::class);
+    }
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+    public function likes(){
+        return $this->hasMany(Like::class);
     }
 }
