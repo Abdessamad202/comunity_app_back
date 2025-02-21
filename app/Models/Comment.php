69c68f78c3b72871d574f6420bc73684e91b3218
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-22 00:57:06 +0100

    Update app/Models/Comment.php: [Describe the changes. E.g., Added new attributes, modified relationships]

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,8 +9,9 @@ class Comment extends Model
 {
     //
     use HasFactory;
-    public function profile(){
-        return $this->belongsTo(Profile::class);
+    protected $fillable = ['content','user_id','post_id'];
+    public function user(){
+        return $this->belongsTo(User::class);
     }
     public function post(){
         return $this->belongsTo(Post::class);
