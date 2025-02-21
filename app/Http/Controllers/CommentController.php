21177b667315b1b4d2a9f25993ff4b34343a281a
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-22 00:57:04 +0100

    Update app/Http/Controllers/CommentController.php: [Describe the changes. E.g., Implemented new comment creation logic, updated comment retrieval]

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,41 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CommentRequest;
 use App\Models\Comment;
+use App\Models\Post;
+use App\Models\Profile;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Support\Facades\Auth;
 
-class CommentController extends Controller
+class CommentController extends Controller implements HasMiddleware
 {
+    public static function middleware()
+    {
+        return [new Middleware('auth:sanctum')];
+    }
     /**
      * Display a listing of the resource.
      */
-    public function index()
+
+    public function showComments(Post $post)
     {
-        //
+        $comments = $post->comments()->with('user.profile')->paginate(10);
+
+        return response()->json([
+            'comments' => $comments->items(), // التعليقات ديال الصفحة الحالية
+            'nextPage' => $comments->nextPageUrl(), // الرابط ديال الصفحة الجاية
+            'total' => $post->comments()->count(), // عدد التعليقات الكلي
+        ]);
     }
+
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CommentRequest $request, Post $post)
     {
-        //
-    }
+        // $comment = Comment::create($request->validated());
+        Comment::create(
+            array_merge($request->validated(), ['user_id' => Auth::id(), 'post_id' => $post->id])
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Comment $comment)
-    {
-        //
+        );
+        return response()->json(['message' => 'Comment created!']);
     }
-
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Comment $comment)
+    public function update(CommentRequest $request, Comment $comment)
     {
         //
+        $comment->update($request->validated());
+        return response()->json(['message' => 'Comment updated!']);
     }
 
     /**
@@ -44,6 +60,7 @@ class CommentController extends Controller
      */
     public function destroy(Comment $comment)
     {
-        //
+        $comment->delete();
+        return response()->json(['message' => 'Comment deleted!']);
     }
 }
