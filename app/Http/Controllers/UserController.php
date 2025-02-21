f9bed7a17752603e17918a90085a4b82ccf4db2d
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-22 00:57:28 +0100

    Add app/Http/Controllers/UserController.php: Initial implementation of user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
+
+class UserController extends Controller implements HasMiddleware
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public static function middleware(){
+        return [new Middleware('auth:sanctum')];
+    }
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+        $user->load('profile');
+        return $user;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
